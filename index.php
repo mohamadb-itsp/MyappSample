4d41e076829b2ac23f7e39e9ed011fab584a72fa
Mohamad Berjawi <mohamadb@itsp365.com>
2020-08-28 23:31:53 +0300

    Added php

---
+++ index.php
@@ -0,0 +1,14 @@
+<<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>My App</title>
+  </head>
+  <body>
+  <?php
+
+    echo "Hello, World!"
+
+  ?>
+  </body>
+</html>
